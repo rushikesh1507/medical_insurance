160f890ae3872544a20c1dcd2f7293285be13d6f
rushikesh1507 <pardeshirushikesh07@gmail.com>
2023-07-05 12:45:06 +0530

    Added project configuration file

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+
+PORT_NUMBER = 5004
+
+# MONGODB_PORT_NUMBER = 27017
+# MONGODB_DATABASE_NAME = 'project_db'
+MODEL_FILE_PATH =  os.getcwd() +  r'\static\models\medical_ins_linear_regression.pkl'
+
+JSON_FILE_PATH =   os.getcwd() +  r'\static\json_files\proj_data1.json'
