71ae410342b181a3197c0bd0866150cdf46a9675
rushikesh1507 <pardeshirushikesh07@gmail.com>
2023-07-06 16:12:03 +0530

    config updated

--- config.py
+++ config.py
@@ -2,9 +2,6 @@ import os
 
 
 PORT_NUMBER = 8080
-
-
 MODEL_FILE_PATH =  os.path.join( r'artifacts','medical_ins_linear_regression.pkl')
-
 JSON_FILE_PATH =   os.path.join( r'artifacts','proj_data1.json')
 
